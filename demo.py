bcf2dbde626f4152df82c5ff230fd168c1b7000b
Axel Ivan Segura Ramos <axel.segurarms@uanl.edu.mx>
2025-04-07 15:50:37 -0600

    prueba clase cuadrado ok

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
 class Cuadrado:
-    pass
+    def __init__(self,lado):
+        self.lado = lado
+    def area (self):
+        return self.lado * self.lado
+
+lado = int(input("Ingrese el lado"))
+ejemplo = Cuadrado (lado)
+r = ejemplo.area()
+print(f"el area es {r}")
+
